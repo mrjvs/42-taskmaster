af9162cd15e24ef1b330d9e3f332a2a75666e238
Harm Smits <hsmits@student.codam.nl>
2022-05-25 14:38:17 +0200

    Jelle wants me to push borken code

--- src/options.ts
+++ src/options.ts
@@ -1,4 +1,26 @@
+export type OptionDescriptor = string;
+export type OptionCallable = CallableFunction;
+export type OptionValues = Array<string>
+export type OptionCallableTest = (value: any, key: string) => boolean;
+export type OptionTest = OptionValues|OptionCallableTest;
+
+interface OptionDescription {
+    description: OptionDescription;
+    on: OptionCallable;
+    test: OptionTest;
+}
+
+export type InputOption = [OptionDescriptor, string|OptionDescription|OptionCallable];
+export type Option = Record<OptionDescriptor, OptionDescription>
+
 class Options {
+    protected _options: InputOption[] = [];
+
+    public options(options: InputOption[]) {
+        options.map(opt => {
+
+        })
+    }
 }
 
 export class ClientOptions extends Options {
